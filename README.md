61ff5a4b47ccc210c9edfde1a2cf0f9990a651c9
Simo Elalj <simo@elalj.com>
2021-04-19 01:07:57 +0100

    Fixing Inital commit issues

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # @tonoid/logger
 
+[![Twitter URL](https://img.shields.io/twitter/url/http/shields.io.svg?style=social)](https://github.com/melalj/tonoid-logger)
+[![GitHub stars](https://img.shields.io/github/stars/melalj/tonoid-logger.svg?style=social&label=Star&maxAge=2592003)]()
+
+[![npm](https://img.shields.io/npm/dt/@tonoid/logger.svg)]() [![npm](https://img.shields.io/npm/v/@tonoid/logger.svg)]() [![npm](https://img.shields.io/npm/l/@tonoid/logger.svg)]() [![David](https://img.shields.io/david/melalj/tonoid-logger.svg)]()
+
 Winston plugin for [@tonoid/helpers](https://github.com/melalj/tonoid-helpers).
 
 ## Exported context attributes
@@ -28,4 +33,4 @@ const logger = require('@tonoid/logger');
 
 ## Credits
 
-This module is maintained by [tonoid](https://www.tonoid.com)
+This module is maintained by [Simo Elalj](https://twitter.com/simoelalj) @[tonoid](https://www.tonoid.com)
--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 /* eslint-disable no-param-reassign */
 const winston = require('winston');
 
-const init = () => async () => {
-  const isProd = (
-    process.env.NODE_ENV === 'production'
-    || process.env.NODE_ENV === 'staging'
-  );
-  const isTest = (process.env.NODE_ENV === 'test');
-
-  let level = isProd ? 'info' : 'debug';
-  if (process.env.LOG_LEVEL) level = process.env.LOG_LEVEL;
-
-  const gkeFormatter = winston.format((info) => {
-    info.severity = info.level.toUpperCase();
-    delete info.level;
-    return info;
-  });
-
-  const format = isProd
-    ? winston.format.combine(
-      gkeFormatter(),
-      winston.format.json(),
-    )
-    : winston.format.combine(
-      winston.format.colorize(),
-      winston.format.prettyPrint(),
-      winston.format.splat(),
-      winston.format.simple(),
+module.exports = () => ({
+  name: 'logger',
+  init: () => {
+    const isProd = (
+      process.env.NODE_ENV === 'production'
+      || process.env.NODE_ENV === 'staging'
     );
+    const isTest = (process.env.NODE_ENV === 'test');
 
-  const logger = winston.createLogger({
-    format,
-    level: isTest ? [] : level,
-    transports: [
-      new winston.transports.Console(),
-    ],
-  });
+    let level = isProd ? 'info' : 'debug';
+    if (process.env.LOG_LEVEL) level = process.env.LOG_LEVEL;
 
-  return {
-    info: logger.info,
-    error: logger.error,
-    warn: logger.warn,
-    debug: logger.debug,
-    winston,
-    format,
-  };
-};
+    const gkeFormatter = winston.format((info) => {
+      info.severity = info.level.toUpperCase();
+      delete info.level;
+      return info;
+    });
 
-const loggerModule = module.exports = init; // eslint-disable-line no-multi-assign
-loggerModule.name = 'logger';
+    const format = isProd
+      ? winston.format.combine(
+        gkeFormatter(),
+        winston.format.json(),
+      )
+      : winston.format.combine(
+        winston.format.colorize(),
+        winston.format.prettyPrint(),
+        winston.format.splat(),
+        winston.format.simple(),
+      );
+
+    const logger = winston.createLogger({
+      format,
+      level: isTest ? [] : level,
+      transports: [
+        new winston.transports.Console(),
+      ],
+    });
+
+    return {
+      info: logger.info.bind(logger),
+      error: logger.error.bind(logger),
+      warn: logger.warn.bind(logger),
+      debug: logger.debug.bind(logger),
+      winston,
+      format,
+    };
+  },
+});
